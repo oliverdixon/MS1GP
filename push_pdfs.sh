cd7424d3696bc5e77b149c86fb269e0807985156
Oliver Dixon <od641@york.ac.uk>
2023-03-09 20:49:40 +0000

    Minor: Added FS sanity check in PDF uploader.
    I have also slightly changed the Discord notification format. Rendering
    the entire commit message in an italic typeface is unnecessary.

--- push_pdfs.sh
+++ push_pdfs.sh
@@ -14,10 +14,21 @@ EXPORT_FILES=(
         ${GIT_ROOT}proposal/proposal.pdf
 )
 
+if [[ ! -d $DEST_DIR ]]; then
+        echo -e "The destination directory doesn't exist! Are you on a sane" \
+                "York networked filesystem?\nDEST_DIR=\"$DEST_DIR\""
+        exit 1
+fi
+
+# I don't want to uniformly preprocess files with GhostScript here, since it can
+# be counterproductive or ineffective on some files (e.g. Beamer presentations).
+# We'll leave it up to the user to supply the correctly optimised files for
+# uploading!
+
 for file in ${EXPORT_FILES[@]}; do
         destfile="${DEST_DIR}$(basename "$file")"
 
-        [[ ! -f "$destfile" || "$file" -nt "$destfile" ]] &&
+        [[ ! -f $destfile || $file -nt $destfile ]] &&
                 cp -v "$file" "$destfile"
 done
 
