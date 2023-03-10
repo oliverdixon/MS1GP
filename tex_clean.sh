33403d6784e62114a668eece036ff17063503751
Oliver Dixon <od641@york.ac.uk>
2023-03-10 02:04:12 +0000

    Minor typographical fixes.

--- tex_clean.sh
+++ tex_clean.sh
@@ -3,12 +3,13 @@
 
 # Moves all the LaTeX temporary files, as defined by .gitignore patterns, to a
 # directory called '.tex_trash/' in the git root. Local directory structures
-# (w.r.t the git root) of slated files are mirrored in the trash directory.
+# (w.r.t the git root) of slated files are mirrored in the trash directory. PDF
+# documents are not slated.
 
 GIT_ROOT="$(git rev-parse --show-toplevel)/"
 TRASH_DIR="$GIT_ROOT.tex_trash/"
 TRASH_FILES="$(git ls-files -io --directory --full-name --exclude-standard | \
-        grep -v '^\..*')" 
+        grep -v -e '^\..*' -v -e '\.pdf$')"
 
 # Create the required directory structure.
 xargs -r dirname <<< "$TRASH_FILES" | uniq | while read dir; do
