d6d095c1569fe83310ec0a9dcc5dc933138984aa
Oliver Dixon <od641@york.ac.uk>
2023-03-11 19:53:20 +0000

    Added the majority of Haskell Functors.

--- report/haskell/Functor.hs
+++ report/haskell/Functor.hs
@@ -1,15 +1,7 @@
 type Functor :: (* -> *) -> Constraint
-class Functor f where
-  fmap :: (a -> b) -> f a -> f b
-  (<$) :: a -> f b -> f a
-  {-# MINIMAL fmap #-}
-        -- Defined in ‘GHC.Base’
-instance Functor (Either a) -- Defined in ‘Data.Either’
-instance Functor [] -- Defined in ‘GHC.Base’
-instance Functor Solo -- Defined in ‘GHC.Base’
-instance Functor Maybe -- Defined in ‘GHC.Base’
-instance Functor IO -- Defined in ‘GHC.Base’
-instance Functor ((->) r) -- Defined in ‘GHC.Base’
-instance Functor ((,,,) a b c) -- Defined in ‘GHC.Base’
-instance Functor ((,,) a b) -- Defined in ‘GHC.Base’
-instance Functor ((,) a) -- Defined in ‘GHC.Base’
+class Functor (f :: * -> *) where
+        fmap :: (a -> b) -> ( f a -> f b )
+        (<$) :: a -> f b -> f a
+
+instance Functor [] -- The list constructor is an exemplary Functor!
+
