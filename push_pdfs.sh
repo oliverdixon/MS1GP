e53a6b133e942c5e3735082d92ddbbcea71c3a8a
Oliver Dixon <od641@york.ac.uk>
2023-03-13 06:09:13 +0000

    Minor: removed extraneous files from the uploader.

--- push_pdfs.sh
+++ push_pdfs.sh
@@ -13,7 +13,6 @@ EXPORT_FILES=(
         ${GIT_ROOT}presentation/presentation.pdf
         ${GIT_ROOT}presentation/speaker_notes/notes.pdf
         ${GIT_ROOT}report/report.pdf
-        ${GIT_ROOT}report/owd_report.pdf
         ${GIT_ROOT}proposal/proposal.pdf
 )
 
